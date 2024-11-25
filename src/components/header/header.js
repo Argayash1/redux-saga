5fbe5268b479269cc1d31fd99fdea96797d828c1
zilberman_yai <zilberman_yai@sima-land.ru>
2024-11-26 01:01:44 +0500

    lesson-9-end-2

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,11 @@
 import { NavLink } from "react-router-dom";
 
-import './header.css';
+import "./header.css";
 
 const navigation = [
-  { id: 1, title: 'Home', path: '/' },
-  { id: 2, title: 'Latest News', path: '/latest-news' },
-  { id: 3, title: 'Popular News', path: '/popular-news' },
+  { id: 1, title: "Home", path: "/" },
+  { id: 2, title: "Latest News", path: "/latest-news" },
+  { id: 3, title: "Popular News", path: "/popular-news" },
 ];
 
 const Header = () => (
@@ -13,7 +13,9 @@ const Header = () => (
     <nav className="nav">
       <div className="links">
         {navigation.map(({ id, title, path }) => (
-          <NavLink key={id} to={path} exact={true}>{title}</NavLink>
+          <NavLink key={id} to={path}>
+            {title}
+          </NavLink>
         ))}
       </div>
     </nav>
